61ceba992cff7723ede9363ba63f371b2ad09b6f
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-01-20 10:45:53 -0800

    a

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -16,13 +16,13 @@ void swap_int(int *a, int *b)
 }
 
 /**
- * build_max_heapify - Make a special kind of heap from a tree to  organized.
+ * max_heapify - Make a special kind of heap from a tree to  organized.
  * @array: the array of integers
  * @size: size of the array
  * @parent: the base row in the tree.
  * @child: the root row in the tree.
  */
-void build_max_heapify(int *array, size_t size, size_t parent, size_t child)
+void max_heapify(int *array, size_t size, size_t parent, size_t child)
 {
 	size_t max_heap, mini_heap;
 
@@ -33,13 +33,13 @@ void build_max_heapify(int *array, size_t size, size_t parent, size_t child)
 	{
 		swap_int(&array[max_heap], &array[parent]);
 		print_array(array, size);
-		build_max_heapify(array, size, parent, max_heap);
+		max_heapify(array, size, parent, max_heap);
 	}
 	if (child != mini_heap)
 	{
 		swap_int(&array[mini_heap], &array[child]);
 		print_array(array, size);
-		build_max_heapify(array, size, child, mini_heap);
+		max_heapify(array, size, child, mini_heap);
 	}
 }
 
@@ -58,13 +58,13 @@ void heap_sort(int *array, size_t size)
 	for (z = 0; z <= (size / 2) - 1; z++)
 	{
 
-	build_max_heapify(array, size, 0, size - 1);
+	max_heapify(array, size, 0, size - 1);
 	}
 
 	for (z = 0; z < size - 1; z++)
 	{
 		swap_int(array + z, array + (size - 1));
 		print_array(array, size);
-		build_max_heapify(array, size, z, 0);
+		max_heapify(array, size, z, 0);
 	}
 }
