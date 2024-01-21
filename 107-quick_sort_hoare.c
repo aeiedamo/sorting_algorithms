ba2785c085e95986116789fed0bf26dd2b8e6335
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-01-21 11:39:35 +0300

    Add #11 quick sort with Hoare

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,80 +1,53 @@
+#include "needed.c"
 #include "sort.h"
 
+static int *main_array;
+int main_size, set = 0;
+
 /**
- * swap_int - swaps two elements in array
- * @a: pointer to the first element to swap
- * @b: pointer to the second element to swap
+ * _partition_ - function to partition an array using lomuto scheme
+ * @array: pointer to array;
+ * @low: the lower bound
+ * @high: the higher bound
+ * Return: the new index of pivot
  */
 
-void swap_int(int *a, int *b)
-{
-	int tempNumber;
+int _partition_(int *array, int low, int high) {
+  int *pivot = (array + low), i, j;
 
-	tempNumber = *a;
-	*a = *b;
-	*b = tempNumber;
+  for (i = low - 1, j = high + 1;;) {
+    do {
+      ++i;
+    } while (*(array + i) < *pivot);
+    do {
+      --j;
+    } while (*(array + j) > *pivot);
+    if (i >= j)
+      return j;
+    swap((array + i), (array + j));
+    print_array(main_array, main_size);
+  }
 }
 
 /**
- * hoare_partition - Organize numbers using the Hoare Partitioning method
- * @array: the array of integers
- * @size: size of the array
- * @minimum: the first element in the array
- * @maximum: the last element in the array
- * Return: The index after partitioning.
- */
-int hoare_partition(int *array, size_t size, int maximum, int minimum)
-{
-	int pivot = array[maximum], left = array[minimum], z;
-
-	for (z = minimum; z <= maximum - 1; z++)
-	{
-		do {
-			maximum++;
-		} while (array[z] < pivot);
-		do {
-			left--;
-		} while (pivot < array[minimum]);
-
-		if (left != minimum)
-		{
-			swap_int(&array[maximum], &array[minimum]);
-			print_array(array, size);
-		}
-	}
-
-	return (z);
-}
-
-/**
- * hoare_sort - Sort elements recursively with quicksort.
- * @array: the array of integers
- * @size: size of the array
- * @minimum: the first element in the array
- * @maximum: the last element in the array
- */
-void hoare_sort(int *array, size_t size, int maximum, int minimum)
-{
-	int half;
-
-	if (maximum -  minimum > 0)
-	{
-		half = hoare_partition(array, size, minimum, maximum);
-		hoare_sort(array, size, minimum, half - 1);
-		hoare_sort(array, size, half, maximum);
-	}
-}
-
-/**
- * quick_sort_hoare - Arrange numbers in ascending order using the quick sort
- * @array: the array of integers
- * @size: the size of the array
- * Return: 0
+ * quick_sort_hoare - function to sort an array with quick sort algorithm
+ * @array: pointer to array
+ * @size: size of array
  */
 
-void quick_sort_hoare(int *array, size_t size)
-{
-	if (array == 0 || size < 2)
-		return;
-	hoare_sort(array, size, 0, size - 1);
+void quick_sort_hoare(int *array, size_t size) {
+  int p;
+
+  if (!array || size < 2)
+    return;
+
+  if (set == 0) {
+    main_array = array;
+    main_size = size;
+    set = 1;
+  }
+
+  p = _partition_(array, 0, size - 1);
+  quick_sort_hoare(array, p + 1);
+  quick_sort_hoare(array + p + 1, size - p - 1);
 }
