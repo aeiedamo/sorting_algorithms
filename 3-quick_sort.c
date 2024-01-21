5b950996909dc4a97baf4802cf3435982b1647b3
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-01-21 09:51:24 +0300

    Fix for #3 quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,38 +1,57 @@
 #include "needed.c"
 #include "sort.h"
 
-size_t main_size, set = 0;
-int *main_array;
+static int *main_array;
+int main_size, set = 0;
 
-int quick_sort0(int *array, size_t start, size_t end) {
-  int *pivot = array + end;
-  size_t i, j;
+/**
+ * _partition_ - function to partition an array using lomuto scheme
+ * @array: pointer to array;
+ * @low: the lower bound
+ * @high: the higher bound
+ * Return: the new index of pivot
+ */
 
-  i = start;
+int _partition_(int *array, int low, int high) {
+  int *pivot = (array + high);
+  int i = low - 1, j;
 
-  for (j = start; j < end; j++) {
+  for (j = low; j < high; j++) {
     if (*(array + j) <= *pivot) {
+      ++i;
+      if (i == j)
+        continue;
       swap((array + j), (array + i));
-      i++;
+      print_array(main_array, main_size);
     }
   }
-  swap((array + i), pivot);
-  print_array(main_array, main_size);
+  ++i;
+  if (*(array + i) != *(array + high)) {
+    swap((array + i), (array + high));
+    print_array(main_array, main_size);
+  }
   return i;
 }
 
+/**
+ * quick_sort - function to sort an array with quick sort algorithm
+ * @array: pointer to array
+ * @size: size of array
+ */
+
 void quick_sort(int *array, size_t size) {
-  int pivot_index;
+  int p;
 
   if (!array || size < 2)
     return;
 
   if (set == 0) {
-    main_size = size;
     main_array = array;
+    main_size = size;
     set = 1;
   }
-  pivot_index = quick_sort0(array, 0, size - 1);
-  quick_sort(array, pivot_index);
-  quick_sort(array + pivot_index + 1, size - pivot_index - 1);
+
+  p = _partition_(array, 0, size - 1);
+  quick_sort(array, p);
+  quick_sort(array + p + 1, size - p - 1);
 }
