93747615138cd8808e88d15b1ce7797edd0bda6e
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-01-23 21:34:41 +0300

    Add src directory and move mandatory tasks

---
+++ src/0-bubble_sort.c
@@ -0,0 +1,21 @@
+#include "needed.c"
+/**
+ * bubble_sort - function that uses bubble sort algorithm to sort an array
+ * @array: pointer to array
+ * @size: size of array
+ */
+
+void bubble_sort(int *array, size_t size) {
+  unsigned int i, j;
+
+  if (size < 2 || array == NULL)
+    return;
+  for (i = 0; i < size - 1; i++) {
+    for (j = 0; j < size - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(&array[j], &array[j + 1]);
+        print_array(array, size);
+      }
+    }
+  }
+}
